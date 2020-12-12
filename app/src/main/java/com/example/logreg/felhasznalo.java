b09902586c375143e6638322d81780fe057f5ec6
DarkDashPub <terjeinorbert2000@gmail.com>
2020-12-12 22:26:55 +0100

    Felhasznalo osztaly kész

---
+++ app/src/main/java/com/example/logreg/felhasznalo.java
@@ -0,0 +1,33 @@
+package com.example.logreg;
+
+public class felhasznalo {
+
+    private String email;
+    private String username;
+    private String password;
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
